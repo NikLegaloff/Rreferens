cebf32060b35855509e2e33253895b9d51d1e82d
Nik <niklegaloff@gmail.com>
2025-10-20 17:51:50 +0300

    111

--- Danik.WebUI/wwwroot/js/site.js
+++ Danik.WebUI/wwwroot/js/site.js
@@ -6,20 +6,18 @@ function Draw(m) {
     var html = "";
     var template = structuredClone(m.Template);
     FillTemplate(template, m.PersonInfos, m.Epitaph);
-    console.log(template);
     template.Portraits.forEach(function (o, i) {
         html += AddLayer(o, "<img src='" + GetURL(m.PersonInfos[i].ImageId) + "' class='Portrait'>");
     });
-
+    
     template.Texts.forEach(function (o, i) {
         html += AddLayer(o, "<div style='text-align:" + o.AlignStr + "; font-size:" + GetPx(o.Size) + "; " + (o.Bold ? "font-weight:bold" : "") + "'>" + o.Text + "</div>");
     });
-
-
+    if (template.Images != null && template.Images!=undefined)
     template.Images.forEach(function (o, i) {
         var extra = "";
-        if (o.H != undefined && o.H != null) extra = "height:" + GetPx(o.H) + ";";
-        html += AddLayer(o, "<img style='width:" + GetPx(o.W) + ";" + extra + "' src='" + GetURL(o.ImageId) + "'/>");
+        if (o.Area.H != undefined && o.Area.H != null) extra = "height:" + GetPx(o.Area.H) + ";";
+        html += AddLayer(o, "<img style='width:" + GetPx(o.Area.W) + ";" + extra + "' src='" + GetURL(o.ImageId) + "'/>");
     });
 
 
@@ -29,7 +27,6 @@ function Draw(m) {
 
 function FillTemplate(t, pis, epitaph) {
     var se = t.SingleEpitaph;
-    console.log(t);
     for (var i = 0; i < pis.length; i++) {
         var pi = pis[i];
         //t.Portraits[i].ImageId = pi.ImageId;
@@ -60,93 +57,6 @@ function AddLayer(subj, html) {
 };
 
 
-(function ($) {
-    $.fn.imageSelector = function (images, options = {}) {
-        const settings = $.extend({
-            selectedUrl: null,        // начальная картинка
-            onImageSelected: null     // callback(index, imageUrl)
-        }, options);
-
-        return this.each(function () {
-            const $container = $(this);
-            let currentIndex = 0;
-
-            // если передан selectedUrl — ищем индекс этой картинки
-            if (settings.selectedUrl) {
-                const foundIndex = images.indexOf(settings.selectedUrl);
-                if (foundIndex !== -1) {
-                    currentIndex = foundIndex;
-                }
-            }
-
-            // HTML шаблон
-            const widget = $(`
-        <div class="image-selector-widget">
-          <div class="controls">
-            <button class="btn btn-primary nav-btn prev">⟨ Назад</button>
-
-            <div class="dropdown">
-              <button class="btn btn-outline-secondary dropdown-toggle d-flex align-items-center" type="button" data-bs-toggle="dropdown">
-                <img src="${images[currentIndex]}" alt="preview">
-              </button>
-              <ul class="dropdown-menu"></ul>
-            </div>
-
-            <button class="btn btn-primary nav-btn next">Вперёд ⟩</button>
-          </div>
-        </div>
-      `);
-
-            $container.html(widget);
-
-            const $dropdownMenu = $container.find(".dropdown-menu");
-            const $dropdownButton = $container.find(".dropdown-toggle");
-
-            // Заполняем список миниатюр
-            images.forEach((img, i) => {
-                const $item = $(`
-          <li>
-            <a class="dropdown-item" href="#" data-index="${i}">
-              <img src="${img}" alt="thumb"> Изображение ${i + 1}
-            </a>
-          </li>
-        `);
-                $dropdownMenu.append($item);
-            });
-
-            function updateImage(index) {
-                currentIndex = index;
-                $dropdownButton.find("img").attr("src", images[index]);
-                // Вызов колбэка при выборе
-                if (typeof settings.onImageSelected === "function") {
-                    settings.onImageSelected(index, images[index]);
-                }
-            }
-
-            // Обработчики
-            $dropdownMenu.on("click", ".dropdown-item", function (e) {
-                e.preventDefault();
-                const index = parseInt($(this).data("index"));
-                updateImage(index);
-            });
-
-            $container.find(".prev").on("click", function () {
-                currentIndex = (currentIndex - 1 + images.length) % images.length;
-                updateImage(currentIndex);
-            });
-
-            $container.find(".next").on("click", function () {
-                currentIndex = (currentIndex + 1) % images.length;
-                updateImage(currentIndex);
-            });
-
-            // Вызов события сразу при инициализации (если selectedUrl задан)
-            if (typeof settings.onImageSelected === "function") {
-                settings.onImageSelected(currentIndex, images[currentIndex]);
-            }
-        });
-    };
-})(jQuery);
 
                 /*
 $(function() {
